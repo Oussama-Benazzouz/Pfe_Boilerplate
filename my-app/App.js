817f0135424244b21cbab1d038269f4ea2e8a1a6
Oussama-Benazzouz <oussama.benazzouz@edu.uiz.ac.ma>
2024-03-25 11:04:54 +0000

    Structured project

--- my-app/App.js
+++ my-app/App.js
@@ -2,14 +2,8 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
 
-function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
